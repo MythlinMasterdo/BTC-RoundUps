381c5c92364adc6eaab8a201b20f5737f3ff7e24
MythlinMasterdo <miles71397@gmail.com>
2017-11-26 13:42:34 -0800

    implement config and build skeleton routes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const app = express();
+const config = require('config');
+const PORT = config.get('PORT');
 
-app.listen(3000, () => console.log('listening!'))
+//TODO: call the handlers folder and build out a get transactions method
+app.get('/api/transactions/:username', (req, res) => {
+  console.log('req.query ', req.query, req.params);
+  res.sendStatus(200)
+});
+//TODO: call the handlers folder and build out a add transactions method
+app.put('/api/transactions/:username', (req, res) => {
+  
+});
+//TODO: call the handlers folder and build out add user method
+app.post('/api/users', (req, res) => {
+
+});
+
+app.listen(PORT, () => console.log('listening on', PORT));
